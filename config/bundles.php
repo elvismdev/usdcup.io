2d6a82fc05eb29ca5f0f4770d0f2a300e2e4d27e
Elvis Morales <elvismdev@gmail.com>
2021-11-19 14:43:18 -0800

    Installing ORM package.

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Xynnn\GoogleTagManagerBundle\GoogleTagManagerBundle::class => ['all' => true],
     Ob\HighchartsBundle\ObHighchartsBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
