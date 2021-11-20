fac435b44403d3f8310d7b7e80a2b3e8dd95d0ed
Elvis Morales <elvismdev@gmail.com>
2021-11-19 19:55:06 -0800

    Separating home javascript.

---
+++ assets/js/home.js
@@ -0,0 +1,68 @@
+// Require JS libraries.
+const { CountUp } = require('countup.js');
+
+// import CountUp from 'countup.js';
+
+(function() {
+
+	"use strict";
+
+  // Define an async function
+	async function fetchAsync (endpoint) {
+		let uri = '/';
+		uri += endpoint;
+		let response = await fetch(uri, {
+			method:'GET'
+		});
+		let data = await response.json();
+		return data;
+	}
+
+	// Define countUp function.
+	function countUp(e, start, end, decimalPlaces) {
+		const options = {
+			  startVal: start,
+			  decimalPlaces: decimalPlaces,
+		};
+
+		var countUp = new CountUp(e, end, options);
+		if (!countUp.error) {
+			countUp.start();
+		} else {
+			  console.error(countUp.error);
+		}
+	}
+
+
+	var	$body = document.querySelector('body');
+	var $loadingParagraph = document.getElementById('loading-p');
+
+	// Do stuff after page is fully loaded.
+	window.addEventListener('load', function() {
+		// Play initial animations on page load.
+		window.setTimeout(function() {
+			$body.classList.remove('is-preload');
+		}, 100);
+
+
+
+		// Get and display Average Price.
+		let avPriceElement = document.getElementById('average-price');
+		let adsQtyElement = document.getElementById('ads-qty');
+		fetchAsync('api/get_average_price')
+		.then(function(data) {
+			// Remove loading dots.
+			$loadingParagraph.classList.remove('loading-dots');
+			
+			// Count up from 0 and display value.
+			countUp(avPriceElement, 0.00, data.average_price, 2);
+
+			// Count down from 100.
+			countUp(adsQtyElement, 100, data.total_ads_evaluated, 0);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+	});
+
+})();
