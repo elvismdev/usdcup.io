ea89d0e624edd62da7a22c43118c39c0904fff2e
Elvis Morales <elvismdev@gmail.com>
2021-11-19 22:33:53 -0800

    Creating a Revolico Service.

--- src/Util/UtilityBox.php
+++ src/Util/UtilityBox.php
@@ -2,24 +2,24 @@
 
 namespace App\Util;
 
-
 /**
  * Custom utility methods.
  */
-class UtilityBox 
+class UtilityBox
 {
-	/**
-	 * Add ! prefix to banned words for searching.
-     * @param array $words     Default requests' options
+    /**
+     * Add ! prefix to banned words for searching.
+     * @param array $words Default requests' options
+     *
      * @return array
      */
-	public static function addExclPrefix(array $words = [])
-	{
-		foreach($words as &$value) {
-			$value = '!' . $value;
-		}
-		unset($value);
+    public static function addExclPrefix(array $words = [])
+    {
+        foreach ($words as &$value) {
+            $value = '!'.$value;
+        }
+        unset($value);
 
-		return $words;
-	}
+        return $words;
+    }
 }
