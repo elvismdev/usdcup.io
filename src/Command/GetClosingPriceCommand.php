94b54704a6ccadcda202d2019af8356c45071045
Elvis Morales <elvismdev@gmail.com>
2021-11-19 21:06:30 -0800

    Initialize command to get and record a closing price.

---
+++ src/Command/GetClosingPriceCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\PriceHistory;
+
+class GetClosingPriceCommand extends Command
+{
+    protected static $defaultName = 'app:get-closing-price';
+    protected static $defaultDescription = 'Gets a closing price and records into the Price History entity.';
+
+    protected function configure(): void
+    {
+        // $this
+        //     ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+        //     ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        // ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        // $arg1 = $input->getArgument('arg1');
+
+        // if ($arg1) {
+        //     $io->note(sprintf('You passed an argument: %s', $arg1));
+        // }
+
+        // if ($input->getOption('option1')) {
+        //     // ...
+        // }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
