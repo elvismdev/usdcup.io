2358d739c95388c2b65075a5e05a613a9820f7a5
Elvis Morales <elvismdev@gmail.com>
2022-01-06 20:31:00 -0800

    Create new entity fields to hold min and max ad values and URL at the moment of data fecthing.

---
+++ migrations/Version20220107042921.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220107042921 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE price_history ADD max_price_ad INT NOT NULL, ADD min_price_ad INT NOT NULL, ADD max_price_ad_url VARCHAR(255) NOT NULL, ADD min_price_ad_url VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE price_history DROP max_price_ad, DROP min_price_ad, DROP max_price_ad_url, DROP min_price_ad_url');
+    }
+}
